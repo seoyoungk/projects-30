014427d5afe1fa824db90600cf4a981e8a6bde7a
seoyoungk <vvwvv9@gmail.com>
2019-05-20 18:48:13 +0900

    project28 - SceneDetector_new

--- project28/SceneDetector_new/SceneDetector_new/ViewController.swift
+++ project28/SceneDetector_new/SceneDetector_new/ViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import Vision
+
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var scene: UIImageView!
+    @IBOutlet weak var answerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let image = UIImage(named: "train_night") else {
+            fatalError("Error: invalid image!")
+        }
+        scene.image = image
     }
 
-
+    @IBAction func pickImage(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = true
+        self.present(imagePicker, animated: true)
+    }
+    
 }
 
+extension ViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: false){ () in
+            let alert = UIAlertController(title: "", message: "deselected", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alert, animated: false)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        picker.dismiss(animated: false){ () in
+            let image = info[.editedImage] as? UIImage
+            self.scene.image = image
+        }
+    }
+
+    
+}
